7b7e52f222556c3046fcf6ba2e102d30701bda58
Marcin Jarczewski <marcin.jarczewski@brilliancy.pl>
2020-05-14 22:06:02 +0200

    Add designTime viewModels

---
+++ pdfConverter.WPF/DesignTime/Models/DesignShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using pdfConverter.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplePdfConverter.WPF.DesignTime.Models
+{
+    public class DesignShellViewModel : PropertyChangedBase
+    {
+        public DesignShellViewModel()
+        {
+            MenuItems = new ObservableCollection<ShellMenuItem>();
+            MenuItems.Add(new ShellMenuItem() { Caption = "Convert" });
+            MenuItems.Add(new ShellMenuItem() { Caption = "Settings"});      
+        }
+
+        public ObservableCollection<ShellMenuItem> MenuItems { get; private set; }
+    }
+}
